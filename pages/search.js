ebcde2dd240b8a649d791ad08c19d5daf2c59c63
piotrmigas <piotr.migas@icloud.com>
2021-10-18 19:38:55 +0200

    commit

--- pages/search.js
+++ pages/search.js
@@ -22,7 +22,7 @@ const Search = ({ results }) => {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = true;
+  const useDummyData = false;
   const startIndex = context.query.start || "0";
 
   const { data } = await axios(
